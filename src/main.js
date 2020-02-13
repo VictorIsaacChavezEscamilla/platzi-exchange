62faac3401cf5f87346df3b2fc669be091a4425a
Victor Isaac Chavez Escamilla <victor.chavez@ikongps.com>
2020-02-13 11:05:05 -0600

    Project Kick Off

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App.vue'
+import Chart from 'chart.js'
 
-Vue.config.productionTip = false;
+import '@/assets/css/tailwind.css' //@ = Directory source
+import Chartick from 'vue-chartkick'
+import { VueSpinners } from '@saeris/vue-spinners'
+
+import router from '@/router'
+
+import { dollarFilter, percentFilter } from '@/filters'
+
+Vue.filter('dollar', dollarFilter)
+Vue.filter('percent', percentFilter)
+
+Vue.use(VueSpinners)
+Vue.use(Chartick.use(Chart))
+
+Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+    router,
+    render: h => h(App)
+}).$mount('#app')
